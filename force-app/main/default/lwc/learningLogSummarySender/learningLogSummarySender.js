16bf22724d7b58263a8b4aa42f0d965d128ab6b2
banana9091 <siheonjung9091@gmail.com>
2025-07-23 02:54:02 +0900

    이메일전송 완

--- force-app/main/default/lwc/learningLogSummarySender/learningLogSummarySender.js
+++ force-app/main/default/lwc/learningLogSummarySender/learningLogSummarySender.js
@@ -1,10 +1,12 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class LearningLogFlowLauncher extends LightningElement {
+export default class LearningLogSummarySender extends LightningElement {
     @api recordId;
-    @track isRunning = false;
-    @track isSuccess = false;
-    @track isFailure = false;
+
+    isRunning = false;
+    isSuccess = false;
+    isFailure = false;
+    showFlow = false;
 
     get inputVariables() {
         return [
@@ -16,23 +18,48 @@ export default class LearningLogFlowLauncher extends LightningElement {
         ];
     }
 
-    startFlow() {
-        this.isRunning = true;
+    handleClick() {
         this.isSuccess = false;
         this.isFailure = false;
+        this.isRunning = true;
+        this.showFlow = true;
     }
 
     handleStatusChange(event) {
-        this.isRunning = false;
+        const status = event.detail.status;
 
-        if (event.detail.status === 'FINISHED') {
-            this.isSuccess = true;
-        } else if (
-            event.detail.status === 'FAILED' ||
-            event.detail.status === 'ERROR' ||
-            event.detail.status === 'UNKNOWN'
+        if (status === 'FINISHED') {
+            this.isSuccess = false;
+            this.isFailure = false;
+
+            const outputVars = event.detail.outputVariables || [];
+            const resultVar = outputVars.find(v => v.name === 'emailActuallySent');
+            const wasEmailSent = resultVar?.value === true;
+
+            if (wasEmailSent) {
+                this.isSuccess = true;
+            } else {
+                this.isFailure = true;
+            }
+
+            setTimeout(() => {
+                this.isSuccess = false;
+                this.isFailure = false;
+            }, 5000);
+        }
+
+        if (
+            status === 'FAILED' ||
+            status === 'ERROR' ||
+            status === 'UNKNOWN'
         ) {
+            this.isSuccess = false;
             this.isFailure = true;
+
+            setTimeout(() => {
+                this.isFailure = false;
+            }, 5000);
         }
     }
+
 }
